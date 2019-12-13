310f66ad983ca133670922d8a7e0b9d184f7b1e5
kurniawandenyy <denykurniawan146@gmail.com>
2019-12-13 14:52:21 +0700

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routeNav = require('./src/index')
 const logger = require('morgan')
 const cors = require('cors')
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8000
 
 //use dependencies
 const app = express()
